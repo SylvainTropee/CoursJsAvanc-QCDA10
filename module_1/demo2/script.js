27cd3664be138194ee174817d741e06d51938692
stropee <stropee@campus-eni.fr>
2025-05-26 11:37:41 +0200

    module 1 demo 2

---
+++ module_1/demo2/script.js
@@ -0,0 +1,33 @@
+
+const beers = ['Paix-Dieu', 'La chouffe', 'Kro', 'Maximator', '86'];
+
+//récupérer le select
+const selectBeer = document.getElementById('selectBeer')
+//boucles sur le tableau
+beers.forEach(function (value){
+    let option = document.createElement('option')
+    option.value = value.toLowerCase()
+    option.innerText = value
+    selectBeer.append(option)
+})
+let p = document.createElement('p')
+document.body.append(p)
+selectBeer.addEventListener("change", function(){
+    console.log(this)
+    //let p = document.createElement('p')
+    p.innerText = this.value
+    p.style.textTransform = "capitalize"
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
