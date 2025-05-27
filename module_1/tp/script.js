200070a51c141d288df841a4a8d929b75682b3cb
stropee <stropee@campus-eni.fr>
2025-05-27 09:39:00 +0200

    module 1 TP

--- module_1/tp/script.js
+++ module_1/tp/script.js
@@ -1,16 +1,17 @@
 import {dico} from "./dico.js";
 
+let tries = 5
 let randomWord = getRandomWord()
 console.log(randomWord)
 
-function getRandomWord(){
+function getRandomWord() {
     let num = Math.floor(Math.random() * dico.length)
     return dico[num];
 }
 
-function generateLetters(){
+function generateLetters() {
 
-    for(let i = "A".charCodeAt(0); i <= "Z".charCodeAt(0); i++){
+    for (let i = "A".charCodeAt(0); i <= "Z".charCodeAt(0); i++) {
         const letter = String.fromCharCode(i)
         const button = document.createElement('button')
         button.innerText = letter
@@ -21,18 +22,50 @@ function generateLetters(){
     }
 }
 
-function displayWord(){
+function displayWord() {
     const wordToFind = document.getElementById('wordToFind')
     wordToFind.innerText = randomWord.replace(/[A-Z]/gi, "_")
 }
 
-function checkLetter(){
+function checkLetter() {
     console.log(this.value)
+    let isLetterCorrect = false;
+    let randomDomWord =
+        document.getElementById('wordToFind').innerText.split("")
+    console.log(randomDomWord)
+    for (let i = 0; i < randomWord.length; i++) {
+        if (randomWord[i] === this.value) {
+            randomDomWord[i] = this.value
+            isLetterCorrect = true
+        }
+    }
+    document.getElementById('wordToFind').innerText = randomDomWord.join("")
+
+    if (!isLetterCorrect) {
+        document.getElementById('tries').innerText = --tries
+    }
+
+    checkwin()
 }
 
-function init(){
+function checkwin() {
+    let randomDomWord =
+        document.getElementById('wordToFind').innerText
+    if (randomDomWord === randomWord && tries > 0) {
+        document.getElementById('wordToFind').innerText = `Bravo c'est gagné ! Le mot était ${randomWord}`
+    }
+
+    if (tries <= 0) {
+        alert("T'es un looser ! Rejoue !")
+    }
+}
+
+function init() {
     generateLetters()
     displayWord()
+    document.getElementById('reset').addEventListener("click", function (){
+        window.location.reload()
+    })
 }
 
 window.onload = init
